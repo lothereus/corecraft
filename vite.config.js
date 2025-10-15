55e9c12e92a33bf80cfbfb327e003f89749a6d45
lothereus <rolland.delhorbe@gmail.com>
2025-10-15 22:25:20 +0200

    website add english version (no js)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        en: resolve(__dirname, 'index-en.html'),
+      },
+    },
   },
 })
